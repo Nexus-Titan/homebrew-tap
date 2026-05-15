6b4f51c00d81e52cd2cee5ec9ada5387e9589435
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-05-15 20:28:18 +0200

    Fix SHA256 checksum for NexusTitan formula
    Updated SHA256 checksum for version 4.1.1.

--- Formula/nexus-titan.rb
+++ Formula/nexus-titan.rb
@@ -2,7 +2,7 @@ class NexusTitan < Formula
   desc "C++ based interpreter for the Nexus language"
   homepage "https://tiwut.org"
   url "https://github.com/Nexus-Titan/Nexus-the-programming-language/archive/refs/tags/V4.1.1.tar.gz"
-  sha256 "2b56b1bff0f32d573d42054268e1b74710b46eef877321d236f264bdfe115ee6"
+  sha256 "f8be7ddcc12865fdfea4bc88e42b000b7ae2ed9cf69523be92741932a8b319be"
   license "MIT"
   version "4.1.1"
   
