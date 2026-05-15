f43ba76fc359476e74474a113de4d6f120652ec8
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-05-15 20:27:30 +0200

    Rename class to NexusTitan and update SHA256

--- Formula/nexus-titan.rb
+++ Formula/nexus-titan.rb
@@ -1,17 +1,18 @@
-class Nexus < Formula
+class NexusTitan < Formula
   desc "C++ based interpreter for the Nexus language"
   homepage "https://tiwut.org"
   url "https://github.com/Nexus-Titan/Nexus-the-programming-language/archive/refs/tags/V4.1.1.tar.gz"
-  sha256 "625476542754274257245gfhghwezt453342"
+  sha256 "2b56b1bff0f32d573d42054268e1b74710b46eef877321d236f264bdfe115ee6"
   license "MIT"
   version "4.1.1"
-
+  
   def install
     system "g++", "-o", "nexus", "interpreter.cpp"
+    
     bin.install "nexus"
   end
 
   test do
-    assert_match "Nexus", shell_output("#{bin}/nexus --version", 2)
+    assert_predicate bin/"nexus", :exist?
   end
 end
