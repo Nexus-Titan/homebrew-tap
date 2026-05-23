99f0a085f1e73345614a49036a313398d7c8738e
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-05-23 14:01:17 +0200

    Update tiwut-network-scanner.rb

--- Formula/tiwut-network-scanner.rb
+++ Formula/tiwut-network-scanner.rb
@@ -1,29 +1,29 @@
 class TiwutNetworkScanner < Formula
   desc "Advanced Network Scanner"
   homepage "https://github.com/tiwut/Network-Scanner"
-  url "https://codeload.github.com/tiwut/Network-Scanner/tar.gz/refs/tags/v2.4.6"
-  sha256 "756292e4cee517289ee946dfed9afff8bf2031d41735e5d9b71292f6dded58c9"
-  license "MIT"
   version "2.4.6"
+  license "MIT"
 
-  env :std
+  if OS.mac?
+    url "https://codeload.github.com/tiwut/Network-Scanner/tar.gz/refs/tags/v2.4.6"
+    sha256 "756292e4cee517289ee946dfed9afff8bf2031d41735e5d9b71292f6dded58c9"
 
-  depends_on "cmake" => :build
-  depends_on "qt"
-
-  on_linux do
-    depends_on "gcc"
+    depends_on "cmake" => :build
+    depends_on "qt"
+  elsif OS.linux?
+    url "https://github.com/tiwut/Network-Scanner/releases/download/v2.4.6/Network-Scanner-x86_64.AppImage"
+    sha256 "756292e4cee517289ee946dfed9afff8bf2031d41735e5d9b71292f6dded58c9"
   end
 
   def install
-    args = std_cmake_args + %W[
-      -DCMAKE_PREFIX_PATH=#{Formula["qt"].opt_prefix}
-    ]
-
-    system "cmake", "-S", ".", "-B", "build", *args
-    system "cmake", "--build", "build"
-    
-    bin.install "build/Network-Scanner"
+    if OS.mac?
+      system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+      system "cmake", "--build", "build"
+      bin.install "build/Network-Scanner"
+    elsif OS.linux?
+      appimage = Dir["*.AppImage"].first
+      bin.install appimage => "Network-Scanner"
+    end
   end
 
   test do
