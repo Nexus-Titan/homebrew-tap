14559a933d8c64210408b43966713d5f12527f1e
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-05-23 14:39:29 +0200

    Update tiwut-terminal.rb

--- Formula/tiwut-terminal.rb
+++ Formula/tiwut-terminal.rb
@@ -1,19 +1,30 @@
 class TiwutTerminal < Formula
   desc "Ultra-fast, cross-platform command-line emulator built in C++ and Qt 6"
   homepage "https://github.com/tiwut/Tiwut-Terminal"
-  url "https://codeload.github.com/tiwut/Tiwut-Terminal/tar.gz/refs/tags/v2.3.3"
-  sha256 "d8016ed97253e660d7f64228be47b239e94a6978f631eb30d48ad14a2133cd9f"
+  version "2.3.3"
   license "MIT"
-  version "2.3.3" 
-  
-  depends_on "cmake" => :build
-  depends_on "qt"
+
+  if OS.mac?
+    url "https://codeload.github.com/tiwut/Tiwut-Terminal/tar.gz/refs/tags/v2.3.3"
+    sha256 "d8016ed97253e660d7f64228be47b239e94a6978f631eb30d48ad14a2133cd9f"
+    
+    depends_on "cmake" => :build
+    depends_on "qt"
+  elsif OS.linux?
+    url "https://github.com/tiwut/Tiwut-Terminal/releases/download/v2.3.3/Tiwut_Terminal-x86_64.AppImage"
+    sha256 "d8016ed97253e660d7f64228be47b239e94a6978f631eb30d48ad14a2133cd9f"
+  end
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    system "cmake", "--build", "build"
-    
-    bin.install "build/Tiwut-Terminal"
+    if OS.mac?
+      system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+      system "cmake", "--build", "build"
+      
+      bin.install "build/Tiwut-Terminal"
+    elsif OS.linux?
+      appimage = Dir["*.AppImage"].first
+      bin.install appimage => "Tiwut-Terminal"
+    end
     
     (bin/"exec").mkpath
   end
