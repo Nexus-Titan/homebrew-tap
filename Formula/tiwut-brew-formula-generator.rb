37355d87a3346ba8b1e8a104ae1b2bb040192b6f
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-05-24 15:32:35 +0200

    Update tiwut-brew-formula-generator.rb

--- Formula/tiwut-brew-formula-generator.rb
+++ Formula/tiwut-brew-formula-generator.rb
@@ -3,21 +3,31 @@ class TiwutBrewFormulaGenerator < Formula
   homepage "https://github.com/tiwut/Brew-Formula-Generator"
   license "MIT"
   version "1.1.1"
-  url "https://github.com/tiwut/Brew-Formula-Generator/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "c3f6d07957ce8d63c54ce6c2826cefc3ef53329e732dab0d6aa40c3c49d1e430"
 
-  depends_on "cmake" => :build
-  depends_on "qt"
+  if OS.mac?
+    url "https://github.com/tiwut/Brew-Formula-Generator/archive/refs/tags/v1.1.1.tar.gz"
+    sha256 "c3f6d07957ce8d63c54ce6c2826cefc3ef53329e732dab0d6aa40c3c49d1e430"
+
+    depends_on "cmake" => :build
+    depends_on "qt"
+  elsif OS.linux?
+    url "https://github.com/tiwut/Network-Scanner/releases/download/v2.4.6/Network_Scanner-x86_64.AppImage"
+    sha256 "ad6a549f12807d003cea078820aa702f30b808236191647701489dfcad5a674d"
+  end
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
+    if OS.mac?
+      system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+      system "cmake", "--build", "build"
+      bin.install "build/BrewFormulaGenerator"
+    elsif OS.linux?
+      appimage = Dir["*.AppImage"].first
+      bin.install appimage => "BrewFormulaGenerator"
+    end
   end
 
   test do
     assert_predicate bin/"BrewFormulaGenerator", :exist?
     assert_predicate bin/"BrewFormulaGenerator", :executable?
   end
-
 end
