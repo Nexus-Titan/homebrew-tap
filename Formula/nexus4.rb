a9815ede9fc4de25a6d0b48ffa843182624ce234
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-05-25 20:54:09 +0200

    Update nexus4.rb

--- Formula/nexus4.rb
+++ Formula/nexus4.rb
@@ -4,9 +4,10 @@ class Nexus4 < Formula
   url "https://github.com/Nexus-Titan/Nexus-the-programming-language/archive/refs/tags/v4.5.0.tar.gz"
   sha256 "94c8456e808f4ac657cc384bb812f07ce4adc1d1703199eda1b8f502ca7ebc80"
   license "MIT"
+  
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-
+  depends_on "xorgproto"
   depends_on "libx11"
   depends_on "openssl@3"
 
