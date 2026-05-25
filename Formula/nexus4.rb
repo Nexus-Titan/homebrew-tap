7d18987342d94cc18ead775687d8ebfb8860e8a9
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-05-25 13:38:40 +0200

    Update Nexus version to 4.1.3

--- Formula/nexus4.rb
+++ Formula/nexus4.rb
@@ -1,8 +1,8 @@
 class Nexus4 < Formula
   desc "C++ based interpreter for the Nexus language"
   homepage "https://tiwut.org/nexus"
-  url "https://github.com/Nexus-Titan/Nexus-the-programming-language/archive/refs/tags/V4.1.2.tar.gz"
-  sha256 "2ceb09792c34aeb4261a14d2a1808d8b522e94f0683481ca3895d73bc8f588e4"
+  url "https://github.com/Nexus-Titan/Nexus-the-programming-language/archive/refs/tags/v4.1.3.tar.gz"
+  sha256 "3de48d7930141c986826f9397cd34a2cb8226acabff481e5015a3a20a85253ef"
   license "MIT"
 
   def install
