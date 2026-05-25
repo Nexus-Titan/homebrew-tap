1be30ab54414a958b7b3e04ee9939c3e8c041512
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-05-25 21:19:13 +0200

    Update nexus4.rb

--- Formula/nexus4.rb
+++ Formula/nexus4.rb
@@ -6,23 +6,67 @@ class Nexus4 < Formula
   license "MIT"
   
   depends_on "cmake" => :build
-  depends_on "libx11"
-  depends_on "xorgproto"
   depends_on "openssl@3"
+  on_linux do
+    depends_on "libx11"
+  end
 
+  on_macos do
+  end
   def install
-    inreplace "CMakeLists.txt" do |s|
-      s.gsub! "find_package(X11 REQUIRED)", "# find_package(X11 REQUIRED) bypassed"
-      s.gsub! "${X11_INCLUDE_DIR}", "#{Formula["libx11"].opt_include} #{Formula["xorgproto"].opt_include}"
-      s.gsub! "${X11_LIBRARIES}", "#{Formula["libx11"].opt_lib}/#{shared_library("libX11")}"
-    end
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    system "cmake", "--build", "build"
+    openssl = Formula["openssl@3"]
+
+    cmake_args = std_cmake_args + [
+      "-DCMAKE_BUILD_TYPE=Release",
+      "-DOPENSSL_ROOT_DIR=#{openssl.opt_prefix}",
+      "-DOPENSSL_INCLUDE_DIR=#{openssl.opt_include}",
+      "-DOPENSSL_LIBRARIES=#{openssl.opt_lib}",
+    ]
     
+    if OS.mac?
+      xquartz = "/opt/X11"
+      if File.directory?(xquartz)
+        cmake_args += [
+          "-DX11_INCLUDE_DIR=#{xquartz}/include",
+          "-DX11_LIBRARIES=#{xquartz}/lib/libX11.dylib",
+        ]
+      end
+    end
+
+    system "cmake", "-S", ".", "-B", "build", *cmake_args
+    system "cmake", "--build", "build", "--config", "Release"
+
     bin.install "nexus" => "nexus4"
+
+    doc.install "README.md", "SYNTAX.md"
+    (share/"nexus4/Samples").install Dir["Samples/*"] if File.directory?("Samples")
   end
 
   test do
-    assert_predicate bin/"nexus4", :executable?
+    (testpath/"hello.nx").write <<~NX
+      print("Hello from Nexus!")
+    NX
+    assert_match "Hello from Nexus!", shell_output("#{bin}/nexus4 #{testpath}/hello.nx")
+  end
+
+  def caveats
+    msg = <<~EOS
+      Nexus #{version} has been installed as `nexus4`.
+
+      Run a script:
+        nexus4 script.nx
+
+    EOS
+
+    if OS.mac?
+      msg += <<~EOS
+        ⚠  macOS note: The GUI and networking features rely on X11 (XLib).
+           Please install XQuartz if you need GUI windows:
+             https://www.xquartz.org
+           After installing, log out and back in so DISPLAY is set.
+      EOS
+    end
+
+    msg
   end
 end
