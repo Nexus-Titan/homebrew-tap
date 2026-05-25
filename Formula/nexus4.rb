b8f7c5f3a6b8c01c07e7895c5dcb76fb5e71fc79
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-05-25 20:51:20 +0200

    Add pkg-config dependency and update CMake args
    Added pkg-config as a build dependency and updated CMake arguments to include X11 paths.

--- Formula/nexus4.rb
+++ Formula/nexus4.rb
@@ -5,12 +5,20 @@ class Nexus4 < Formula
   sha256 "94c8456e808f4ac657cc384bb812f07ce4adc1d1703199eda1b8f502ca7ebc80"
   license "MIT"
   depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+
   depends_on "libx11"
   depends_on "openssl@3"
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    args = std_cmake_args + %W[
+      -DX11_X11_INCLUDE_PATH=#{HOMEBREW_PREFIX}/include
+      -DX11_X11_LIB=#{HOMEBREW_PREFIX}/lib/#{shared_library("libX11")}
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args
     system "cmake", "--build", "build"
+    
     bin.install "nexus" => "nexus4"
   end
 
