2a2f486d428b6a689b6a3f29c0dbdd797252fca1
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-05-26 16:50:30 +0200

    Update tiwut-cli.rb

--- Formula/tiwut-cli.rb
+++ Formula/tiwut-cli.rb
@@ -2,7 +2,7 @@ class TiwutCli < Formula
   desc "Tiwut Terminal TUI Desktop Dashboard"
   homepage "https://github.com/tiwut/Tiwut-CLI"
   url "https://github.com/tiwut/Tiwut-CLI/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "ae8c05c442a2e19c6f6b8e046ca439bc533c5c407eb6cc37a74e0ea12d480c62"
   head "https://github.com/tiwut/Tiwut-CLI.git", branch: "main"
 
   depends_on "cmake" => :build
