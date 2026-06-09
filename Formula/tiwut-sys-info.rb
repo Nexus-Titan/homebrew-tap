0cf673372f956b3c949dacd5c32cfe6d6dae03ec
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-06-09 20:04:56 +0200

    Update tiwut-sys-info.rb

--- Formula/tiwut-sys-info.rb
+++ Formula/tiwut-sys-info.rb
@@ -2,7 +2,7 @@ class TiwutSysInfo < Formula
   desc "Comprehensive vertical system information script for Unix-like OSs"
   homepage "https://github.com/tiwut/Tiwut-Sys-Info"
   url "https://github.com/tiwut/Tiwut-Sys-Info/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "ae8c05c442a2e19c6f6b8e046ca439bc533c5c407eb6cc37a74e0ea12d480c62"
+  sha256 "15f35e21722605e9256ff797ffc2a4cf2a663b329d2bb749ded564aff5332800"
   license "MIT"
 
   def install
